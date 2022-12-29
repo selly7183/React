fce0b1dec54e8ea9e16cfd8e3297c6c6bbacc1dc
selly7183 <selly7183@naver.com>
2022-12-29 18:00:05 +0900

    Context

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import AppWrap from "./AppWrap";
+import AppTheme from "./AppTheme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<AppWrap />
+		<AppTheme />
 	</React.StrictMode>
 );
 
