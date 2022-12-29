88a2f07d90c3dbdfae4262ac9b8cc0bfe88b897e
selly7183 <selly7183@naver.com>
2022-12-29 14:26:32 +0900

    useReducer

--- src/AppMentors.jsx
+++ src/AppMentors.jsx
@@ -1,40 +1,27 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+import personReducer from "./reducer/person-reducer";
 
 export default function AppMentors() {
-	const [person, setPerson] = useState(initialPerson);
+	const [person, dispatch] = useReducer(personReducer, initialPerson);
 
 	const handleUpdate = () => {
 		const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
 		const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
 
-		setPerson((person) => ({
-			...person,
-			mentors: person.mentors.map((mentor) => {
-				if (mentor.name === prev) {
-					return { ...mentor, name: current };
-				}
-				return mentor;
-			}),
-		}));
+		dispatch({ type: "updated", prev, current });
 	};
 
 	const handleAdd = () => {
 		const name = prompt(`멘토의 이름은?`);
 		const title = prompt(`멘토의 직함은?`);
 
-		setPerson((person) => ({
-			...person,
-			mentors: [...person.mentors, { name, title }],
-		}));
+		dispatch({ type: "added", name, title });
 	};
 
 	const handleDelete = () => {
 		const name = prompt(`누구를 삭제하고 싶은가요?`);
 
-		setPerson((person) => ({
-			...person,
-			mentors: person.mentors.filter((mentor) => mentor.name !== name),
-		}));
+		dispatch({ type: "deleted", name });
 	};
 
 	return (
